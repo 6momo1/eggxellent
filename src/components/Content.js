63fed410f5df0290edfedb674a18df53cee60506
tomi <tomilui1221@gmail.com>
2022-03-22 14:45:07 -0700

    update cdn path

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import egg1 from '../images/egg1.jpeg'
-import egg2 from '../images/egg2.jpg'
+import { images } from '../data';
 const Content = () => {
     return (
         <>
             <div className="flex flex-col justify-center items-center 
     bg-white h-screen font-mono py-40 mb-20
     ">
-                <img src={egg1} className="h-full rounded mb-20 shadow" />
+                <img src={images.eggbenedict} className="h-full rounded mb-20 shadow" />
                 <div className='flex flex-col justify-center items-center mb-10'>
                     <h2 className='text-3xl mb-2'>Egg Toast</h2>
                     <p className='mb-2 text-2xl'>Crispy, delicious and nutritious</p>
@@ -17,7 +16,7 @@ const Content = () => {
             <div className="flex flex-col justify-center items-center 
     bg-white h-screen font-mono py-40 mb-20
     ">
-                <img src={egg2} className="h-full rounded mb-20 shadow" />
+                <img src={images.eggtoast} className="h-full rounded mb-20 shadow" />
                 <div className='flex flex-col justify-center items-center mb-20'>
                     <h2 className='text-3xl mb-2'>Egg Avocado</h2>
                     <p className='mb-2 text-2xl'>Juicy, creamy and delicious</p>
---
+++ src/data.js
@@ -0,0 +1,4 @@
+export const images = {
+    eggbenedict : `https://react-tomi-lui.s3.us-west-2.amazonaws.com/eggbenedict.png`,
+    eggtoast: `https://react-tomi-lui.s3.us-west-2.amazonaws.com/eggtoast.jpg`
+}
